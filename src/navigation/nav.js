69ee3975e399e5daf47d7bd56c2f92d95651244b
pradeep-08 <114543921+pradeep-08@users.noreply.github.com>
2023-11-19 17:51:45 +0530

    deploying frontEnd

--- src/navigation/nav.js
+++ src/navigation/nav.js
@@ -6,10 +6,10 @@ import "./nav.css"
   return <nav>
     <div className="nav-container">
         <input     onChange={handleInputChange}
-          value={query} type="text" placeholder="Enter your search" className = "search-input" /> 
+          value={query} type="text" placeholder="Enter search in Dark-Cart" className = "search-input" /> 
     </div>
     <div className="profile-container">
-    
+    <img width="18" height="18" src="https://img.icons8.com/parakeet/48/user.png" alt="user"/>
     </div>
   </nav>
 }
